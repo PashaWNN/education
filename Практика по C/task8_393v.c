29a95dc92e06d81a6121feef02d2b95460b2d288
Pavel Shishmarev <pashawnn@gmail.com>
2018-03-22 23:48:07 +0900

    edit tasks, lecture, cheatsheet

---
+++ Практика по C/task8_393v.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  int[n*n] matrix;
+  for (int i = 0; i<n*n; i++) {
+    scanf("%d", &n[i]);
+  }
+  for (i = (n*n)-1; i>=0; i--) {
+    if (flag==1) {
+      sum += matrix[i]
+    }
+  }
+  return 0;
+}
